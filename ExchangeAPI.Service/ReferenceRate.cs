dfdf1e9e314d246d3b152eb9357a5dafc6c4083b
Vabka <vab-999@ya.ru>
2021-03-30 16:36:59 +0500

    Uneccessary record -> class

--- ExchangeAPI.Service/ReferenceRate.cs
+++ ExchangeAPI.Service/ReferenceRate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ExchangeAPI.Service
 {
-    public record ReferenceRate
+    public class ReferenceRate
     {
         public DateTimeOffset Date { get; init; }
         public IReadOnlyCollection<CurrencyRate> Rates { get; init; } = null!;
